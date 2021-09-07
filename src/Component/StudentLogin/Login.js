847abd0bd58f2757816334998b91931fbb0078a4
shtainoortariq <asaadjawed70@gmail.com>
2021-09-07 19:55:45 +0500

    SettingAuthinstudent

--- src/Component/StudentLogin/Login.js
+++ src/Component/StudentLogin/Login.js
@@ -1,10 +1,57 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import { auth } from '../../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+
+
+
+
 
 
 const StudentLogin =()=>{
 
-const [showStudentForm ,setshowStudentForm] = useState(false)
+const [showStudentForm ,setshowStudentForm] = useState(false);
+
+//for signin student
+const [studentLogInEmail, setstudentLogInEmail] = useState(" ");
+const [studentPass, setstudentPass] = useState(" ");
+
+
+//for signup student
+const [studentSignupEmail, setstudentSignupEmail] = useState(" ");
+const [studentsignUpPass, setstudentsignUpPass] = useState(" ");
+
+//for history push
+
+const history = useHistory();
+
+
+  const handleSignIn = async () => {
+      signInWithEmailAndPassword(auth, studentLogInEmail, studentPass).then(res =>{
+
+          history.push()
+
+      }).catch(err =>{
+
+        "Errror found In SignIn authentication"
+      })
+  }
+
+
+  const signUp =() => {
+    return(
+
+      auth.createUserWithEmailAndPassword(studentSignupEmail, studentsignUpPass).then(res =>{
+
+      }).catch(err =>{
+        "Error found In Signup authentication"
+      })
+      
+    )
+  }
+
 
     return(
         <div>
@@ -18,15 +65,15 @@ const [showStudentForm ,setshowStudentForm] = useState(false)
               <div>
 
               <div className="form-group">
-                <input type="email" className="email" placeholder="Email" /> <br /> <br />
-                <input type="password" className="email" placeholder="Password" />
+                <input type="email"  className="email" placeholder="Email" value={studentLogInEmail} onChange={ e  => setstudentLogInEmail (e.target.value)} /> <br /> <br />
+                <input type="password" className="email" placeholder="Password" value={studentPass} onChange={ e => setstudentPass( e.target.value)} />
               </div>
 
               <input type="checkbox" className="Check" defaultValue="checkbox" /> 
               <label className="Remember">Remember Me</label>
 
 
-              <button type="Login" className="btn">Login</button>
+              <Button type="Login" onClick={handleSignIn} className="btn">Login</Button>
               <br />
               <a href="#" className="forget">Forget Password?</a>
               </div>
@@ -43,10 +90,10 @@ const [showStudentForm ,setshowStudentForm] = useState(false)
           <div className="form-group">
           <input type="text" className="email" placeholder="First Name" /><br /> <br />
           <input type="text" className="email" placeholder="Last Name" /><br /> <br />
-          <input type="email" className="email" placeholder="Email" /><br /> <br />
-                <input type="password" className="email" placeholder="Password" /> <br /> <br />
+          <input type="email" className="email" placeholder="Email" value={studentSignupEmail} onChange={e => setstudentSignupEmail(e.target.value)}  /><br /> <br />
+                <input type="password" className="email" placeholder="Password" value={studentsignUpPass} onChange={ e=> setstudentsignUpPass(e.target.value)} /> <br /> <br />
                 <input type="number" className="email" placeholder="Contact No" />
-                <button type="Account" className="btn3">Create Account</button>
+                <Button type="Account" className="btn3">Create Account</Button>
               </div>
         </div>
         </div>
