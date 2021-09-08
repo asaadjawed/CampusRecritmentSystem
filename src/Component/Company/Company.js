9f889557ff0dc1d71abfc5c2fe491afb29b07ef4
shtainoortariq <asaadjawed70@gmail.com>
2021-09-08 19:38:51 +0500

    CompanyLoginForm

--- src/Component/Company/Company.js
+++ src/Component/Company/Company.js
@@ -1,57 +1,102 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import { auth } from '../../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
-const CompanyLogin =()=>{
 
-  const [showCompanyForm ,setshowCompanyForm] = useState(false)
+const CompanyLogin = () => {
 
-    return(
-        <div>
-  
-        <div className="row">
+
+
+  const [showCompanyForm, setshowCompanyForm] = useState(false);
+
+  //for signin student
+  const [CompanyLogInEmail, setCompanyLogInEmail] = useState(" ");
+  const [companyPass, setcompanyPass] = useState(" ");
+
+
+  //for signup student
+  const [companySignupEmail, setstudentSignupEmail] = useState(" ");
+  const [companysignUpPass, setstudentsignUpPass] = useState(" ");
+
+  //for history push
+
+  const history = useHistory();
+
+
+  const handleSignIn = async () => {
+    signInWithEmailAndPassword(auth, CompanyLogInEmail, companyPass).then(res => {
+
+      history.push('/CompanyDashboard')
+
+    }).catch(err => {
+
+      "Errror found In SignIn authentication"
+    })
+  }
+
+
+  const handleSignUp = async () => {
+
+    createUserWithEmailAndPassword(auth, companySignupEmail, companysignUpPass).then(res => {
+
+      history.push()
+
+    }).catch(err => {
+      "Error found In Signup authentication"
+    })
+
+  }
+
+
+  return (
+    <div>
+
+      <div className="row">
 
         <div className="col login_form">
-              <h1 className="LoginHeading">Company Log in</h1>
-              <hr className= "FirstLine"/>
-              <div>
+          <h1 className="LoginHeading">Company Log in</h1>
+          <hr className="FirstLine" />
+          <div>
 
-              <div className="form-group">
-                <input type="email" className="email" placeholder="Email" /> <br /> <br />
-                <input type="password" className="email" placeholder="Password" />
-              </div>
+            <div className="form-group">
+              <input type="email" className="email" placeholder="Email" value={CompanyLogInEmail} onChange={e => setCompanyLogInEmail(e.target.value)} /> <br /> <br />
+              <input type="password" className="email" placeholder="Password" value={companyPass} onChange={e => setcompanyPass(e.target.value)} />
+            </div>
 
-              <input type="checkbox" className="Check" defaultValue="checkbox" /> 
-              <label className="Remember">Remember Me</label>
+            <input type="checkbox" className="Check" defaultValue="checkbox" />
+            <label className="Remember">Remember Me</label>
 
 
-              <button type="Login" className="btn">Login</button>
-              <br />
-              <a href="#" className="forget">Forget Password?</a>
-              </div>
+            <Button type="Login" onClick={handleSignIn} className="btn">Login</Button>
+            <br />
+            <a href="#" className="forget">Forget Password?</a>
+          </div>
         </div>
 
 
         <div className="col SignUp_form">
-          <h1 onClick={()=>{setshowCompanyForm(!showCompanyForm)}} className="SignUpHeading">Create Account</h1>
+          <h1 onClick={() => { setshowCompanyForm(!showCompanyForm) }} className="SignUpHeading">Create Account</h1>
           <h1 className="AllversionText">Company SignUp</h1>
-          <hr className= "SecondLine"/>
+          <hr className="SecondLine" />
 
-          <div value={showCompanyForm} style={{display:showCompanyForm ? 'block': 'none'}}>
-          <div className="form-group">
-          <input type="text" className="email" placeholder="First Name" /><br /> <br />
-          <input type="text" className="email" placeholder="Last Name" /><br /> <br />
-          <input type="email" className="email" placeholder="Email" /><br /> <br />
-                <input type="password" className="email" placeholder="Password" /> <br /> <br />
-                <input type="number" className="email" placeholder="Contact No" />
-                <button type="Account" className="btn3">Create Account</button>
-              </div>
+          <div value={showCompanyForm} style={{ display: showCompanyForm ? 'block' : 'none' }}>
+            <div className="form-group">
+              <input type="text" className="email" placeholder="First Name" /><br /> <br />
+              <input type="text" className="email" placeholder="Last Name" /><br /> <br />
+              <input type="email" className="email" placeholder="Email" value={companySignupEmail} onChange={(e)=>{setstudentSignupEmail(e.target.value)}} /><br /> <br />
+              <input type="password" className="email" placeholder="Password" value={companysignUpPass} oncha /> <br /> <br />
+              <input type="number" className="email" placeholder="Contact No" />
+              <button type="Account" onClick={handleSignUp} className="btn3">Create Account</button>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-        </div>
-  
-    )
+      </div>
+    </div>
+
+  )
 }
 
 export default CompanyLogin
